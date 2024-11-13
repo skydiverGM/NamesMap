c1800762fad81ef801d7719cbe56e17724108ba5
skydiverGM <sonytouchscreen@GMAIL.COM>
2024-11-13 23:44:28 +0100

    Init commit

---
+++ src/main/java/com/imaks/DataHandler.java
@@ -0,0 +1,27 @@
+package com.imaks;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DataHandler {
+
+    Map<Integer, String> map = new DataRepository().getData();
+
+    // Метод формує виведення нумерованого переліку імен
+    public String getAll() {
+        StringBuilder sb = new StringBuilder();
+        AtomicInteger count = new AtomicInteger(0);
+        map.forEach((id, name) ->
+                sb.append(String.format("%d) %d, %s%n",
+                        count.incrementAndGet(),id, name)
+        ));
+        return "\nALL NAMES:\n" + sb;
+    }
+
+    // Метод формує виведення імені за певним id
+    public String getById(int id) {
+        if (map.containsKey(id) && map.get(id) != null) {
+            return String.format("NAME: id %d, %s", id, map.get(id));
+        } else return "No data!";
+    }
+}
---
+++ src/main/java/com/imaks/UIOperator.java
@@ -0,0 +1,8 @@
+package com.imaks;
+
+public class UIOperator {
+
+    public void getOutput(String output) {
+        System.out.println(output);
+    }
+}
